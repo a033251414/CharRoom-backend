a66a6e4a2da1805e55861656ddcbb086fd284fbf
meijyou <a0625141433@gmail.com>
2025-06-13 23:04:38 +0800

    第一次提交

--- obj/Release/net8.0/Server.AssemblyInfo.cs
+++ obj/Release/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85968a5525e9c2a0278929070b8a373ce3cb97fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+775b55dbd4c7bd01957f06915702e65ca33d0f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
