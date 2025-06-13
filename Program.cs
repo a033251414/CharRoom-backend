b418235d19d246e2d6c9dc81faf1cfdce80513bf
meijyou <a0625141433@gmail.com>
2025-06-13 22:21:36 +0800

    Heroku deploy

--- Program.cs
+++ Program.cs
@@ -53,9 +53,12 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173")  
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
+            policy.WithOrigins(
+                    "http://localhost:5173",
+                    "https://a033251414.github.io"  
+                )
+                .AllowAnyHeader()
+                .AllowAnyMethod();
         });
 });
 
@@ -69,10 +72,11 @@ if (app.Environment.IsDevelopment())
 }
 //JWT
 app.UseAuthentication();
+app.UseCors("AllowReactApp");
 app.UseAuthorization();
 
 app.UseHttpsRedirection();
-app.UseCors("AllowReactApp");
+
 app.MapControllers();
 
 app.Run();
