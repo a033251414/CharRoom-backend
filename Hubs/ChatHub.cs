d2976819946d06a09f56ae14ae5fe2720e72ae01
meijyou <a0625141433@gmail.com>
2025-06-24 17:09:00 +0800

    更新收回訊息即時更新

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -3,24 +3,31 @@ using Microsoft.AspNetCore.SignalR;
 namespace Server.Hubs
 {
     public class ChatHub : Hub
+    {
+        // 用戶加入某個群組
+        public async Task JoinGroup(string groupId)
         {
-           // 用戶加入某個群組
-    public async Task JoinGroup(string groupId)
-        {
-           await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
-           Console.WriteLine($"{Context.ConnectionId} joined group {groupId}");
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
+            Console.WriteLine($"{Context.ConnectionId} joined group {groupId}");
         }
 
-    // 用戶離開某個群組（可選）
-    public async Task LeaveGroup(string groupId)
+        // 用戶離開某個群組（可選）
+        public async Task LeaveGroup(string groupId)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupId);
             Console.WriteLine($"{Context.ConnectionId} left group {groupId}");
         }
-    // 傳訊息給群組    
-    public async Task SendMessage(string groupId, ChatMessage message)
+        // 傳訊息給群組    
+        public async Task SendMessage(string groupId, ChatMessage message)
         {
             await Clients.Group(groupId).SendAsync("ReceiveMessage", message);
         }
+        //更新收回訊息
+        public async Task RecallMessage(string groupId, string messageId)
+        {
+            await Clients.Group(groupId).SendAsync("ReceiveRecalledMessage", messageId);
+        }
     }
+    
+    
 }
